1beb905c1b206f3c02897b2aedce1617f7b730d4
Christopher Meyer <christopher_meyer@outlook.de>
2022-08-26 13:35:30 +0200

    feature: add possibility to establish a connection between discord and the bot (#1)

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	discordToken := os.Getenv("DISCORD_TOKEN")
+
+	bot, error := discordgo.New("Bot " + discordToken)
+
+	if error != nil {
+		log.Fatalf("Error while starting bot: %s", error)
+	}
+
+	bot.AddHandler(reactOnMessage)
+
+	bot.Identify.Intents = discordgo.IntentGuildMessages
+	// connection will receive only events defined by this intent
+	// Todo: Add intents if needed
+
+	error = bot.Open()
+
+	if error != nil {
+		log.Fatalf("Error while connecting to gateaway: %s", error)
+		return
+	}
+
+	fmt.Printf("The Bot is now running")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	bot.Close()
+
+}
+
+func reactOnMessage(session *discordgo.Session, message *discordgo.MessageCreate) {
+
+	if message.Author.Bot {
+		return
+	}
+
+	if message.Content == "ping" {
+		session.ChannelMessageSend(message.ChannelID, "pong")
+		return
+	}
+
+	if message.Content == "krossekrabbe" {
+		session.ChannelMessageSend(message.ChannelID, "Nein, hier ist Patrick!")
+		return
+	}
+
+}
